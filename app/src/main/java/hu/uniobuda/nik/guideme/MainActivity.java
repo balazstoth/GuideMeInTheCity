4d31f2afad03a0c1e2c5dbe41fa72af1b519f1e3
Balázs Tóth <tothbalazs@live.com>
2017-04-09 20:08:05 +0200

    Developing MainActivity v2

--- app/src/main/java/hu/uniobuda/nik/guideme/MainActivity.java
+++ app/src/main/java/hu/uniobuda/nik/guideme/MainActivity.java
@@ -1,7 +1,9 @@
 package hu.uniobuda.nik.guideme;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,19 +15,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    List<String> list = Arrays.asList(Category.values().toString());
-    String[] t = {"a","b","c"};
+public class MainActivity extends FragmentActivity
+{
+    List<Category> categories_enum = Arrays.asList(Category.values());
+    List<String> categories_string = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this,list.get(0).toString(),Toast.LENGTH_SHORT).show();
+        CopyCategories();
         Spinner spinnerCategory = (Spinner) findViewById(R.id.spinner_category);
-        ArrayAdapter<String> spinneradapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,t);
-        spinnerCategory.setAdapter(spinneradapter);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, categories_string);
+        spinnerCategory.setAdapter(spinnerAdapter);
+    }
 
+    private void CopyCategories()
+    {
+        for(int i = 0; i < categories_enum.size(); i++)
+            categories_string.add(categories_enum.get(i).toString());
     }
 }
